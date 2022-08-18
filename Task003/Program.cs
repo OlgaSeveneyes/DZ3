113a22d386ae8f6945dd88bd7aae8994e7a2e282
Olga <osa_007@mail.ru>
2022-08-18 23:09:10 +0300

    added task3 for dz3

---
+++ Task003/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+string? num = Console.ReadLine();
+int numI;
+int k = 1;
+
+if (int.TryParse(num, out numI))
+{
+    while (k <= numI)
+    {
+        double cube = Math.Pow(k,3);
+        Console.WriteLine(cube);
+        k++;  
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+} 
